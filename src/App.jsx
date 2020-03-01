dd8d91e1cdaa2c2aca4b018417da10687fe5d962
Gourav Manna <36415100+gouravmanna3@users.noreply.github.com>
2020-03-01 22:54:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {About} from './About'
+import {  Link } from 'react-router-dom';
+class App extends React.Component {
+
+  render() {
+     return (
+        <div>
+          <h1>Home</h1>
+        </div>
+
+     );
+  }
+}
+
+
+export default App;
+
